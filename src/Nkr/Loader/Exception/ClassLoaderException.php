d71aae701867ba88e6a78a2517eb2429c0e82e59
nuukeet <nuukeet@gmail.com>
2011-10-21 02:16:24 +0200

    Initial commit

---
+++ src/Nkr/Loader/Exception/ClassLoaderException.php
@@ -0,0 +1,41 @@
+<?php
+if (! defined('INDEX_CHECK')) exit('No direct script access allowed');
+
+/**
+ * This file is part of the nk-reloaded project 
+ * A full PHP5 conversion of the Nuked-Klan CMS (Experimental)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * @package     Nuukeet
+ * @subpackage  ClassLoader_Exception
+ * @author      nuukeet <nuukeet@gmail.com>
+ * @copyright	Copyright (c) 2011 nk-reloaded project 
+ * @link	https://github.com/nuukeet/nk-reloaded
+ * @version	$Id: $
+ */
+/**
+ * @package     Nuukeet
+ * @subpackage  ClassLoader_Exception
+ * @author      nuukeet <nuukeet@gmail.com>
+ * @copyright	Copyright (c) 2011 nk-reloaded project 
+ */
+
+class ClassLoaderException extends Exception {} {
+    //put your code here
+}
+
+class ClassLoaderMapperException extends Exception {} {
+    //put your code here
+}
+
+?>
