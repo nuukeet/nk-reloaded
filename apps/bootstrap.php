6bfe4179d6616af3a9608334cf9f60e2529e2ef0
nuukeet <nuukeet@gmail.com>
2011-10-21 02:14:15 +0200

    Rename application to apps, create directory config into apps

---
+++ apps/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+define('INDEX_CHECK', true);
+define('LIBS_ROOT', realpath(dirname(__FILE__) . '/../') . '/libs') . '/';
+define('DS', DIRECTORY_SEPARATOR);
+define('PS', PATH_SEPARATOR);
+
+
+require_once realpath(dirname(__FILE__) . '/../') . '/src/Nkr/Loader/ClassMapperLoader.php';
+
